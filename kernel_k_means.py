a5d563111f012be1b1abb8b36bf0c9c9485bcf8b
Yap Wooi Hen <wooihen@live.com>
2015-05-31 01:19:20 +0800

    Added code to compute Gram matrix - RBF kernel.

--- kernel_k_means.py
+++ kernel_k_means.py
@@ -3,10 +3,15 @@ from math import exp
 
 def kernel(data, sigma):
     nData = len(data)
-    Gram = [[0] * nData for i in range(nData)] 
-    # TODO
-    # Calculate the Gram matrix 
+    Gram = [[0] * nData for _ in range(nData)]
 
-    return Gram 
+    # Calculate the Gram matrix
+    for i in range(nData):
+        Gram[i][i] = 1.0
+        for j in range(i+1, nData):
+            Gram[i][j] = exp(-squaredDistance(data[i], data[j])/(2.0*sigma*sigma))
+            Gram[j][i] = Gram[i][j]
+
+    return Gram
 
 
